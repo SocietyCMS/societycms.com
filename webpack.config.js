5c840af4f8851a4feb9136af9db97ef5b12afdf2
Ralph <ralph@huwiler.in>
2016-02-13 13:32:52 +0100

    update webpack config, use filenames

--- webpack.config.js
+++ webpack.config.js
@@ -17,25 +17,19 @@ module.exports = {
 
     module: {
         loaders: [
-            {
-                test: /\.scss$/,
-                loader: 'style!css!sass!postcss'
-            },
-            {
-                test: /\.css$/,
-                loader: "style!css!postcss"
-            },
-            {
-                test: /\.(jpe?g|png|gif|svg)$/i,
-                loaders: [
-                    'file?hash=sha512&digest=hex&name=[hash].[ext]',
-                    'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false!url-loader?limit=100000'
-                ]
-            },
-            {
-                test: /.(woff(2)?|eot|svg|ttf)(\?[a-z0-9=\.]+)?$/,
-                loader: 'url-loader?limit=100000'
-            }
+            {test: /\.scss$/, loader: 'style!css!sass!postcss'},
+            {test: /\.less/, loader: 'style!css!less!postcss'},
+            {test: /\.css$/, loader: "style!css!postcss"},
+
+            {test: /\.png$/, loader: 'url', query: { limit: 25000, prefix: 'img/', name: '[name].[ext]?[hash]'}},
+            {test: /\.jpg$/, loader: 'url', query: { limit: 25000, prefix: 'img/', name: '[name].[ext]?[hash]'}},
+            {test: /\.gif$/, loader: 'url', query: { limit: 25000, prefix: 'img/', name: '[name].[ext]?[hash]'}},
+            {test: /\.svg$/, loader: 'url', query: { limit: 25000, prefix: 'img/', name: '[name].[ext]?[hash]'}},
+
+            {test: /\.(woff(2)?)(\?[a-z0-9=\.]+)?$/, loader: 'url', query: { limit: 25000, prefix: 'font/', name: '[name].[ext]?[hash]'}},
+            {test: /\.(eot)(\?[a-z0-9=\.]+)?$/, loader: 'file', query: { prefix: 'font/', name: '[name].[ext]?[hash]'}},
+            {test: /\.(ttf)(\?[a-z0-9=\.]+)?$/, loader: 'file', query: { prefix: 'font/', name: '[name].[ext]?[hash]'}},
+            {test: /\.(svg)(\?[a-z0-9=\.]+)?$/, loader: 'file', query: { prefix: 'font/', name: '[name].[ext]?[hash]'}}
         ]
     },
     postcss: function () {
