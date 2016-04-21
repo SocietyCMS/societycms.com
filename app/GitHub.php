6198da83bbd15b29308d6df10c066033237e4b35
Ralph Huwiler <ralph+github@huwiler.in>
2016-04-21 15:40:07 -0400

    Applied fixes from StyleCI

--- app/GitHub.php
+++ app/GitHub.php
@@ -8,8 +8,7 @@ use GrahamCampbell\GitHub\Facades\GitHub as GitHubAPI;
 use Illuminate\Support\Facades\Cache;
 
 /**
- * Class GitHub
- * @package App
+ * Class GitHub.
  */
 class GitHub
 {
@@ -18,12 +17,13 @@ class GitHub
      */
     public function latestOrganisationCommits()
     {
-        if (!$this->githubIsConfigured()) {
+        if (! $this->githubIsConfigured()) {
             return [];
         }
 
         return Cache::remember('latestOrganisationCommits', 60, function () {
             $response = GitHubAPI::connection('main')->getHttpClient()->get('orgs/societycms/events');
+
             return collect(ResponseMediator::getContent($response))->where('type', 'PushEvent')->take(3);
         });
     }
@@ -35,18 +35,19 @@ class GitHub
      */
     public function getRepository($vendor, $repository)
     {
-        if (!$this->githubIsConfigured()) {
+        if (! $this->githubIsConfigured()) {
             return [];
         }
 
-        return Cache::remember("repositoryLastUpdated.{$vendor}/{$repository}", 60, function () use($vendor, $repository) {
+        return Cache::remember("repositoryLastUpdated.{$vendor}/{$repository}", 60, function () use ($vendor,$repository) {
             return  GitHubAPI::connection('main')->repo()->show($vendor, $repository);
         });
     }
 
     public function repositoryLastUpdatedAt($vendor, $repository)
     {
-        $repository = $this->getRepository($vendor,$repository);
+        $repository = $this->getRepository($vendor, $repository);
+
         return Carbon::parse($repository['pushed_at'])->diffForHumans();
     }
 
@@ -58,6 +59,7 @@ class GitHub
         if (empty(config('github.connections.main.token'))) {
             return false;
         }
+
         return true;
     }
 }
